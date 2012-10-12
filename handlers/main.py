b03e860d61cb6c56331da1fd31f370a1b14ed591
Jarek Zgoda <jarek.zgoda@gmail.com>
2012-10-12 17:24:59 +0000

    Libs into lib directory

--- handlers/main.py
+++ handlers/main.py
@@ -2,9 +2,10 @@
 
 __revision__ = '$Id$'
 
+import sys
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
 
     def get(self):
-        self.response.write('Hello, world!')
+        self.response.write('Hello, world! sys path: %s' % sys.path)
