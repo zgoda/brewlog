93e7031bd257e954ffec6b17357e391f694b1ad0
Jarek Zgoda <jarek.zgoda@gmail.com>
2012-10-27 18:05:05 +0000

    Brewery ops

--- handlers/brewery.py
+++ handlers/brewery.py
@@ -5,9 +5,16 @@ __revision__ = '$Id$'
 from google.appengine.api import users
 
 from handlers.base import BaseRequestHandler
+from models.base import Brewery
 
 
 class BreweryHandler(BaseRequestHandler):
 
     def list_breweries(self):
-        user = users.get_current_user()
+        current_user = users.get_current_user()
+        bl = Brewery.query(Brewery.user == current_user)
+        ctx = {
+        	'breweries': bl,
+        	'user': current_user,
+        }
+        self.render_to_response('brewery/list.html', **ctx)
