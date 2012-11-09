fde687897f174c09e8f7d2d53437ab77cee11c86
Jarek Zgoda <jarek.zgoda@gmail.com>
2012-11-09 20:33:21 +0000

    Helper methods in models and some template work

--- models/base.py
+++ models/base.py
@@ -23,8 +23,14 @@ class BrewerProfile(db.Model):
         return uri_for('profile-details', keyid=self.key.urlsafe())
 
     @classmethod
-    def get_for_user(cls, user):
-        pass
+    def get_or_create_for_user(cls, user, save=True):
+        try:
+            return cls.query(user==user.key, ancestor=user).fetch(1)[0]
+        except IndexError:
+            profile = cls(parent=user, user=user.key)
+            if save:
+                profile.put()
+            return profile
 
 
 class Brewery(db.Model):
@@ -33,9 +39,26 @@ class Brewery(db.Model):
     description = db.TextProperty()
     description_html = db.TextProperty()
     established_date = db.DateProperty()
+    # alternative established date
+    est_year = db.IntegerProperty()
+    est_month = db.IntegerProperty()
+    est_day = db.IntegerProperty
 
     def get_absolute_url(self):
         return uri_for('brewery-details', keyid=self.key.urlsafe())
 
     def _pre_put_hook(self):
         self.description_html = markdown.markdown(self.description, safe_mode='remove')
+        if self.established_date is not None:
+            self.est_year = self.established_date.year
+            self.est_month = self.established_date.month
+            self.est_day = self.established_date.day
+
+    def get_or_create_for_user(cls, user, save=True):
+        result = cls.query(user=user.key, ancestor=user).fetch_page(20)
+        if not result:
+            brewery = cls(parent=user, user=user.key)
+            if save:
+                brewery.put()
+            result = [brewery]
+        return result
