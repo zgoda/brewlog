59b34566258eb6891e7a2f60da1c3f5eef52301e
Jarek Zgoda <jarek.zgoda@smartupz.com>
2013-04-16 15:55:23 +0200

    Complete setup

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='BrewLog',
     version='0.1',
     long_sescription=__doc__,
-    packages=['brewlog'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+    install_requires=[
+        'pytz',
+        'Flask',
+        'Flask-Babel',
+        'Flask-Script',
+        'Flask-WTForms',
+        'Flask-OAuth',
+        'Markdown',
+        'SQLAlchemy'
+    ]
 )
+
