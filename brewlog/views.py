3c7483848a4c329cefb82a4c95868999a2908f39
Jarek Zgoda <jarek.zgoda@gmail.com>
2013-06-08 08:33:51 +0200

    Show only public data on home page

--- brewlog/views.py
+++ brewlog/views.py
@@ -1,16 +1,15 @@
 from flask import render_template
-
 from brewlog.models import BrewerProfile, Brew, Brewery
 
 
 def main():
     item_limit = 5
     ctx = {
-        'latest_brews': Brew.last_created(limit=item_limit),
-        'latest_breweries': Brewery.last_created(limit=item_limit),
-        'latest_brewers': BrewerProfile.last_created(limit=item_limit),
-        'recently_active_breweries': Brewery.last_updated(limit=item_limit),
-        'recently_active_brewers': BrewerProfile.last_updated(limit=item_limit),
+        'latest_brews': Brew.last_created(limit=item_limit, public_only=True),
+        'latest_breweries': Brewery.last_created(limit=item_limit, public_only=True),
+        'latest_brewers': BrewerProfile.last_created(limit=item_limit, pubic_only=True),
+        'recently_active_breweries': Brewery.last_updated(limit=item_limit, public_only=True),
+        'recently_active_brewers': BrewerProfile.last_updated(limit=item_limit, public_only=True),
     }
     return render_template('home.html', **ctx)
 
