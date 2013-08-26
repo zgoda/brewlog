f036e73abf56dcb6cf20630c73e4880116801ced
Jarek Zgoda <jarek.zgoda@gmail.com>
2013-08-26 15:30:16 +0200

    Even better test running command

--- manage.py
+++ manage.py
@@ -7,30 +7,41 @@ from brewlog import make_app
 from brewlog.db import init_db, clear_db
 
 
-class TestCommand(Command):
+class RunTests(Command):
 
     option_list = (
         Option('labels', nargs='*',
             help='specify individual tests to be run, in form module_name[.TestCaseName[.test_method]]'),
+        Option('-v', '--verbosity', type=int, default=1)
     )
 
-    def run(self, labels):
+    def __init__(self, testdir='', pattern='test_*.py'):
+        self.dirparts = testdir.split('/')
+        self.test_pattern = pattern
+
+    def run(self, labels, verbosity):
         if labels:
-            names = ['brewlog.tests.%s' % name for name in labels]
+            prefix = '.'.join(self.dirparts)
+            if prefix:
+                names = ['%s.%s' % (prefix, name) for name in labels]
+            else:
+                names = labels
             suite = unittest.TestLoader().loadTestsFromNames(names)
         else:
-            suite = unittest.TestLoader().discover('brewlog/tests', pattern='test_*.py')
-        unittest.TextTestRunner(verbosity=2).run(suite)
+            if self.dirparts:
+                testdir = '/'.join(self.dirparts)
+            else:
+                testdir = '.'
+            suite = unittest.TestLoader().discover(testdir, pattern=self.test_pattern)
+        unittest.TextTestRunner(verbosity=verbosity).run(suite)
 
 
 manager = Manager(make_app)
 manager.add_option('-e', '--env', dest='env', default='dev')
 
 manager.add_command('runserver', Server(port=8080))
-
 manager.add_command('shell', Shell())
-
-manager.add_command('test', TestCommand())
+manager.add_command('test', RunTests(testdir='brewlog/tests'))
 
 @manager.command
 def initdb():
