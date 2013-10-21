0a2fe9f8203b404218aa00c65da900e76cf8a7c0
Jarek Zgoda <jarek.zgoda@gmail.com>
2013-10-21 12:40:00 +0200

    This is another session

--- brewlog/users/views/auth.py
+++ brewlog/users/views/auth.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, session, flash, request
 from flask_babel import gettext as _
 from flask_login import login_user, logout_user, login_required
 
-from brewlog import session as dbsession
+from brewlog.db import session as dbsession
 from brewlog.users.auth import services, google, facebook
 from brewlog.models.users import BrewerProfile
 
