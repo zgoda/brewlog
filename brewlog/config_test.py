f225c5c79f9bfa69c848a279e01fa1b2e46d8662
Jarek Zgoda <jarek.zgoda@gmail.com>
2014-07-18 17:22:34 +0200

    Test on in-memory sqlite (why this stopped working?!)

--- brewlog/config_test.py
+++ brewlog/config_test.py
@@ -3,6 +3,7 @@ BABEL_DEFAULT_LOCALE = 'en_US'
 CSRF_ENABLED = False
 WTF_CSRF_ENABLED = CSRF_ENABLED
 LOGIN_DISABLED = False
+SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 AUTH_CONFIG = {
     'google': ('app_id', 'app_secret'),
