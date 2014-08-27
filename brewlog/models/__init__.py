627c01e3460ab18c2d2a92df90656f7b940b6d6b
Jarek Zgoda <jarek.zgoda@gmail.com>
2014-08-27 10:20:27 +0200

    Cosmetic changes to code

--- brewlog/models/__init__.py
+++ brewlog/models/__init__.py
@@ -9,36 +9,41 @@ def breweries(public_only=True, extra_user=None):
     query = Brewery.query
     if public_only:
         if extra_user:
-            query = query.join(BrewerProfile).filter(or_(BrewerProfile.is_public==True, BrewerProfile.id==extra_user.id))
+            query = query.join(BrewerProfile).filter(or_(BrewerProfile.is_public==True, BrewerProfile.id==extra_user.id))  # noqa
         else:
             query = query.join(BrewerProfile).filter(BrewerProfile.is_public==True)
     return query
 
+
 def latest_breweries(ordering, limit=5, public_only=False, extra_user=None):
     return breweries(public_only, extra_user).order_by(desc(ordering)).limit(limit).all()
+
 
 def brews(public_only=True, extra_user=None):
     query = Brew.query
     if public_only:
         query = query.join(Brewery).join(BrewerProfile)
         if extra_user:
-            query = query.filter(or_(BrewerProfile.id==extra_user.id, and_(BrewerProfile.is_public==True, Brew.is_public==True)))
+            query = query.filter(or_(BrewerProfile.id==extra_user.id, and_(BrewerProfile.is_public==True, Brew.is_public==True)))  # noqa
         else:
             query = query.filter(and_(BrewerProfile.is_public==True, Brew.is_public==True))
     return query
 
+
 def latest_brews(ordering, limit=5, public_only=False, extra_user=None):
     return brews(public_only, extra_user).order_by(desc(ordering)).limit(limit).all()
+
 
 def tasting_notes(public_only=True, extra_user=None):
     query = TastingNote.query
     if public_only:
         query = query.join(Brew).join(Brewery).join(BrewerProfile)
         if extra_user:
-            query = query.filter(or_(BrewerProfile.id==extra_user.id, and_(BrewerProfile.is_public==True, Brew.is_public==True)))
+            query = query.filter(or_(BrewerProfile.id==extra_user.id, and_(BrewerProfile.is_public==True, Brew.is_public==True)))  # noqa
         else:
             query = query.filter(and_(BrewerProfile.is_public==True, Brew.is_public==True))
     return query
+
 
 def latest_tasting_notes(ordering, limit=5, public_only=False, extra_user=None):
     return tasting_notes(public_only, extra_user).order_by(desc(ordering)).limit(limit).all()
