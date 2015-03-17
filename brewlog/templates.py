5c777b712181622461b74d227a3e201e39c1d4c0
Jarek Zgoda <jarek.zgoda@gmail.com>
2015-03-17 16:56:02 +0100

    Some cleanups after merge

--- brewlog/templates.py
+++ brewlog/templates.py
@@ -2,7 +2,6 @@ import math
 
 from flask_babelex import format_date
 
-from brewlog.utils import templates
 from brewlog.utils.brewing import plato2sg
 from brewlog.utils.text import stars2deg
 
@@ -14,7 +13,6 @@ def setup_filters(application):
 def setup_globals(application):
     application.jinja_env.globals.update({
         'format_date': format_date,
-        'url_for_other_page': templates.url_for_other_page,
         'pow': math.pow,
         'stars2deg': stars2deg,
         'plato2sg': plato2sg,
