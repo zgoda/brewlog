929ae1f926815b24360a1830e579102fd265ef19
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-02-20 17:51:54 +0100

    More and more tests on bright side

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,9 @@
 import pytest
-from fixture import SQLAlchemyFixture
 from pytest_factoryboy import register
 
 from brewlog import make_app
 from brewlog.ext import db
-from brewlog.models import (
-    Brew, BrewerProfile, Brewery, CustomExportTemplate, CustomLabelTemplate,
-    FermentationStep, TastingNote
-)
 
-from . import fixtures
 from .factories import (
     BreweryFactory, BrewFactory, ExportTemplateFactory,
     FermentationStepFactory, LabelTemplateFactory, TastingNoteFactory,
@@ -30,27 +24,6 @@ def app():
     app = make_app('test')
     with app.app_context():
         db.create_all()
-        env = {
-            'BrewData': Brew,
-            'BreweryData': Brewery,
-            'BrewerProfileData': BrewerProfile,
-            'CustomLabelTemplateData': CustomLabelTemplate,
-            'CustomExportTemplateData': CustomExportTemplate,
-            'TastingNoteData': TastingNote,
-            'FermentationStepData': FermentationStep,
-        }
-        fx = SQLAlchemyFixture(env=env, engine=db.engine)
-        data = fx.data(*[
-            fixtures.BrewData,
-            fixtures.BreweryData,
-            fixtures.BrewerProfileData,
-            fixtures.CustomLabelTemplateData,
-            fixtures.CustomExportTemplateData,
-            fixtures.TastingNoteData,
-            fixtures.FermentationStepData,
-        ])
-        data.setup()
         yield app
-        data.teardown()
         db.session.remove()
         db.drop_all()
--- tests/test_tastingnotes.py
+++ tests/test_tastingnotes.py
@@ -1,48 +1,95 @@
 from flask import url_for
 import pytest
 
-from brewlog.models import Brew, TastingNote, BrewerProfile
+from brewlog.models import TastingNote
 
 from . import BrewlogTests
 
 
-@pytest.mark.usefixtures('client_class')
-class TestTastingNote(BrewlogTests):
+class TastingTests(BrewlogTests):
 
     @pytest.fixture(autouse=True)
-    def set_up(self):
-        self.brew = Brew.query.filter_by(name='pale ale').first()
-        self.hidden_brew_direct = Brew.query.filter_by(name='hidden czech pilsener').first()
-        self.hidden_brew_indirect = Brew.query.filter_by(name='hidden amber ale').first()
-        self.regular_user = BrewerProfile.get_by_email('user@example.com')
+    def set_up(self, tasting_note_factory, brew_factory, brewery_factory, user_factory):
+        self.extra_user = user_factory()
+        # public user data
+        self.public_user = user_factory()
+        self.public_brewery = brewery_factory(
+            brewer=self.public_user, name='public brewery no 1'
+        )
+        self.public_brewery_public_brew = brew_factory(
+            brewery=self.public_brewery, name='public brew no 1'
+        )
+        self.public_brewery_hidden_brew = brew_factory(
+            brewery=self.public_brewery, is_public=False, name='hidden brew no 1'
+        )
 
-    def test_list_public_only(self):
+        # hidden user data
+        self.hidden_user = user_factory(is_public=False)
+        self.hidden_brewery = brewery_factory(
+            brewer=self.hidden_user, name='hidden brewery no 2'
+        )
+        self.hidden_brewery_public_brew = brew_factory(
+            brewery=self.hidden_brewery, name='public brew no 2'
+        )
+        self.hidden_brewery_hidden_brew = brew_factory(
+            brewery=self.hidden_brewery, is_public=False, name='hidden brew no 2'
+        )
+
+        self.list_url = url_for('tastingnote.all')
+
+        self.pbpb_note_1 = tasting_note_factory(
+            brew=self.public_brewery_public_brew,
+            author=self.public_user,
+        )
+        self.pbpb_note_2 = tasting_note_factory(
+            brew=self.public_brewery_public_brew,
+            author=self.extra_user,
+        )
+        self.pbpb_note_3 = tasting_note_factory(
+            brew=self.public_brewery_public_brew,
+            author=self.hidden_user,
+        )
+        self.pbhb_note_1 = tasting_note_factory(
+            brew=self.public_brewery_hidden_brew,
+            author=self.public_user,
+        )
+        self.hbhb_note_1 = tasting_note_factory(
+            brew=self.hidden_brewery_public_brew,
+            author=self.hidden_user,
+        )
+
+        self.ajax_load_url = url_for('tastingnote.loadtext')
+
+
+@pytest.mark.usefixtures('client_class')
+class TestTastingNote(TastingTests):
+
+    def test_list_anon(self):
+        """Anonymous users can see only notes to public brews on the list.
+
         """
-        Regular users can see only notes to public brews on the list
-        """
-        url = url_for('tastingnote.all')
-        rv = self.client.get(url)
+
+        rv = self.client.get(self.list_url)
         page = rv.data.decode('utf-8')
-        assert self.hidden_brew_direct.absolute_url not in page
-        assert self.hidden_brew_indirect.absolute_url not in page
+        assert self.public_brewery_hidden_brew.name not in page
+        assert self.hidden_brewery_public_brew.name not in page
+        assert self.hidden_user.full_name in page
 
-    def test_list_hidden_direct(self):
-        url = url_for('tastingnote.all')
-        self.login(self.hidden_brew_direct.brewery.brewer.email)
-        rv = self.client.get(url)
-        assert self.hidden_brew_direct.absolute_url in rv.data.decode('utf-8')
-
-    def test_list_hidden_indirect(self):
-        url = url_for('tastingnote.all')
-        self.login(self.hidden_brew_indirect.brewery.brewer.email)
-        rv = self.client.get(url)
-        assert self.hidden_brew_indirect.absolute_url in rv.data.decode('utf-8')
+    def test_list_logged_in(self):
+        self.login(self.public_user.email)
+        rv = self.client.get(self.list_url)
+        page = rv.data.decode('utf-8')
+        assert self.public_brewery_public_brew.name in page
+        assert self.public_brewery_hidden_brew.name in page
+        assert self.hidden_user.full_name in page
+        assert self.hidden_brewery_public_brew.name not in page
+        assert self.hidden_brewery_hidden_brew.name not in page
 
     def test_create_anon(self):
         """
         Anonymous users can not add tasting notes
         """
-        url = url_for('tastingnote.add', brew_id=self.brew.id)
+        url = url_for('tastingnote.add', brew_id=self.public_brewery_public_brew.id)
         rv = self.client.get(url)
         assert rv.status_code == 302
 
@@ -50,10 +97,10 @@ class TestTastingNote(BrewlogTests):
         """
         All logged in users can add tasting notes to public brews
         """
-        url = url_for('tastingnote.add', brew_id=self.brew.id)
-        self.login(self.regular_user.email)
+        url = url_for('tastingnote.add', brew_id=self.public_brewery_public_brew.id)
+        self.login(self.extra_user.email)
         rv = self.client.get(url)
-        assert '<form' in rv.data.decode('utf-8')
+        assert 'action="%s"' % url in rv.data.decode('utf-8')
         data = {
             'text': 'Nice beer, cheers!'
         }
@@ -64,8 +111,8 @@ class TestTastingNote(BrewlogTests):
         """
         Users can not add tasting notes to hidden brews
         """
-        url = url_for('tastingnote.add', brew_id=self.hidden_brew_direct.id)
-        self.login(self.regular_user.email)
+        url = url_for('tastingnote.add', brew_id=self.public_brewery_hidden_brew.id)
+        self.login(self.extra_user.email)
         rv = self.client.get(url)
         assert rv.status_code == 403
         data = {
@@ -78,8 +125,8 @@ class TestTastingNote(BrewlogTests):
         """
         Users can not add tasting notes to brews of hidden brewery
         """
-        url = url_for('tastingnote.add', brew_id=self.hidden_brew_indirect.id)
-        self.login(self.regular_user.email)
+        url = url_for('tastingnote.add', brew_id=self.hidden_brewery_public_brew.id)
+        self.login(self.extra_user.email)
         rv = self.client.get(url)
         assert rv.status_code == 403
         data = {
@@ -92,16 +139,20 @@ class TestTastingNote(BrewlogTests):
         """
         Note author can delete it
         """
-        note = TastingNote.create_for(self.brew, self.regular_user, 'Nice beer, cheers!', commit=True)
+        note = TastingNote.create_for(
+            self.public_brewery_public_brew, self.extra_user, 'Nice beer, cheers!', commit=True
+        )
         url = url_for('tastingnote.delete', note_id=note.id)
-        self.login(self.regular_user.email)
+        self.login(self.public_user.email)
         rv = self.client.post(url, data={'delete_it': True}, follow_redirects=True)
         assert note.text not in rv.data.decode('utf-8')
 
     def test_author_sees_delete_form(self):
-        note = TastingNote.create_for(self.brew, self.regular_user, 'Nice beer, cheers!', commit=True)
+        note = TastingNote.create_for(
+            self.public_brewery_public_brew, self.extra_user, 'Nice beer, cheers!', commit=True
+        )
         url = url_for('tastingnote.delete', note_id=note.id)
-        self.login(self.regular_user.email)
+        self.login(self.extra_user.email)
         rv = self.client.get(url)
         assert rv.status_code == 200
         assert 'action="%s"' % url in rv.data.decode('utf-8')
@@ -110,9 +161,11 @@ class TestTastingNote(BrewlogTests):
         """
         Brew owner can delete tasting notes
         """
-        note = TastingNote.create_for(self.brew, self.regular_user, 'Nice beer, cheers!', commit=True)
+        note = TastingNote.create_for(
+            self.public_brewery_public_brew, self.extra_user, 'Nice beer, cheers!', commit=True
+        )
         url = url_for('tastingnote.delete', note_id=note.id)
-        self.login(self.brew.brewery.brewer.email)
+        self.login(self.public_user.email)
         rv = self.client.post(url, data={'delete_it': True}, follow_redirects=True)
         assert note.text not in rv.data.decode('utf-8')
 
@@ -120,41 +173,39 @@ class TestTastingNote(BrewlogTests):
         """
         Not involved in logged users can't delete notes
         """
-        note = TastingNote.create_for(self.brew, self.regular_user, 'Nice beer, cheers!', commit=True)
+        note = TastingNote.create_for(
+            self.public_brewery_public_brew, self.extra_user, 'Nice beer, cheers!', commit=True
+        )
         url = url_for('tastingnote.delete', note_id=note.id)
-        self.login(self.hidden_brew_indirect.brewery.brewer.email)
+        self.login(self.hidden_user.email)
         rv = self.client.post(url, data={'delete_it': True}, follow_redirects=True)
         assert rv.status_code == 403
 
 
 @pytest.mark.usefixtures('client_class')
-class TestTastingNoteAjax(BrewlogTests):
-
-    @pytest.fixture(autouse=True)
-    def set_up(self):
-        self.brew = Brew.query.filter_by(name='pale ale').first()
-        self.regular_user = BrewerProfile.get_by_email('user@example.com')
-        self.url = url_for('tastingnote.loadtext')
+class TestTastingNoteAjax(TastingTests):
 
     def test_load(self):
-        note = TastingNote.create_for(self.brew, self.regular_user, 'Nice beer, cheers!', commit=True)
-        rv = self.client.get(self.url, query_string={'id': note.id})
+        note = TastingNote.create_for(
+            self.public_brewery_public_brew, self.extra_user, 'Nice beer, cheers!', commit=True
+        )
+        rv = self.client.get(self.ajax_load_url, query_string={'id': note.id})
         assert rv.status_code == 200
         assert note.text in rv.data.decode('utf-8')
 
     def test_load_missing_id(self):
-        rv = self.client.get(self.url)
+        rv = self.client.get(self.ajax_load_url)
         assert rv.status_code == 400
 
     def test_load_nonexisting_note(self):
-        rv = self.client.get(self.url, query_string={'id': 666})
+        rv = self.client.get(self.ajax_load_url, query_string={'id': 666})
         assert rv.status_code == 404
 
     def test_anon_cant_edit_notes(self):
         """
         Anonymous user brew details view does not contain js to edit note texts
         """
-        url = url_for('brew.details', brew_id=self.brew.id)
+        url = url_for('brew.details', brew_id=self.public_brewery_public_brew.id)
         edit_url = url_for('tastingnote.update')
         rv = self.client.get(url)
         assert edit_url not in rv.data.decode('utf-8')
@@ -164,14 +215,14 @@ class TestTastingNoteAjax(BrewlogTests):
         Anonymous users can not edit tasting note texts
         """
         note = TastingNote.create_for(
-            self.brew, self.brew.brewery.brewer, 'Nice beer, cheers!', commit=True
+            self.public_brewery_public_brew, self.extra_user, 'Nice beer, cheers!', commit=True
         )
         url = url_for('tastingnote.update')
         data = {
             'pk': note.id,
             'value': 'This brew is horrible!',
         }
-        self.login(self.regular_user.email)
+        self.login(self.hidden_user.email)
         rv = self.client.post(url, data=data)
         assert rv.status_code == 403
 
@@ -179,13 +230,14 @@ class TestTastingNoteAjax(BrewlogTests):
         """
         Author can edit own notes
         """
-        note = TastingNote.create_for(self.brew, self.regular_user, 'Nice beer, cheers!', commit=True)
+        note = TastingNote.create_for(
+            self.public_brewery_public_brew, self.extra_user, 'Nice beer, cheers!', commit=True)
         url = url_for('tastingnote.update')
         data = {
             'pk': note.id,
             'value': 'This brew is horrible!',
         }
-        self.login(self.regular_user.email)
+        self.login(self.extra_user.email)
         rv = self.client.post(url, data=data)
         note = TastingNote.query.get(note.id)
         assert rv.data.decode('utf-8') == note.text_html
@@ -195,26 +247,30 @@ class TestTastingNoteAjax(BrewlogTests):
         """
         Brew owner can edit notes to his brews regardless of note authorship
         """
-        note = TastingNote.create_for(self.brew, self.regular_user, 'Nice beer, cheers!', commit=True)
+        note = TastingNote.create_for(
+            self.public_brewery_public_brew, self.extra_user, 'Nice beer, cheers!', commit=True
+        )
         url = url_for('tastingnote.update')
         data = {
             'pk': note.id,
             'value': 'This brew is horrible!',
         }
-        self.login(self.brew.brewery.brewer.email)
+        self.login(self.public_user.email)
         rv = self.client.post(url, data=data)
         note = TastingNote.query.get(note.id)
         assert rv.data.decode('utf-8') == note.text_html
         assert note.text == data['value']
 
     def test_update_empty_text(self):
-        note = TastingNote.create_for(self.brew, self.regular_user, 'Nice beer, cheers!', commit=True)
+        note = TastingNote.create_for(
+            self.public_brewery_public_brew, self.extra_user, 'Nice beer, cheers!', commit=True
+        )
         url = url_for('tastingnote.update')
         data = {
             'pk': note.id,
             'value': '',
         }
-        self.login(self.brew.brewery.brewer.email)
+        self.login(self.public_user.email)
         rv = self.client.post(url, data=data)
         assert rv.data.decode('utf-8') == note.text_html
 
@@ -223,7 +279,7 @@ class TestTastingNoteAjax(BrewlogTests):
         data = {
             'value': 'This brew is horrible!',
         }
-        self.login(self.regular_user.email)
+        self.login(self.extra_user.email)
         rv = self.client.post(url, data=data)
         assert rv.status_code == 400
 
@@ -233,6 +289,6 @@ class TestTastingNoteAjax(BrewlogTests):
             'pk': 666,
             'value': 'This brew is horrible!',
         }
-        self.login(self.regular_user.email)
+        self.login(self.extra_user.email)
         rv = self.client.post(url, data=data)
         assert rv.status_code == 404
