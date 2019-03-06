d3ff74fb976d4c74389e56cd31c4885f43e3fe66
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-03-07 00:40:09 +0100

    Updated translations

--- update_trans.sh
+++ update_trans.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-pybabel extract -F ./babel.cfg -k lazy_gettext -o ./brewlog/translations/messages.pot ./
+pybabel extract -F ./babel.cfg -k lazy_gettext -o ./brewlog/translations/messages.pot ./brewlog
 pybabel update -l pl -d ./brewlog/translations/ -i ./brewlog/translations/messages.pot
