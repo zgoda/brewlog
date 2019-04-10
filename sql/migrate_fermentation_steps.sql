35826d6fd8da97fdc695da01fbf3f2ae840d9bab
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-04-10 23:28:43 +0200

    Old cruft

--- sql/migrate_fermentation_steps.sql
+++
@@ -1,7 +0,0 @@
-begin;
-insert into fermentation_step(date, name, og, fg, volume, temperature, brew_id) values (
-    select fermentation_start_date, 'primary', og, fg, brew_length, fermentation_temperature, id
-    from brew
-    where fermentation_start_date is not null
-);
-commit;
