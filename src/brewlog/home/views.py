bb4926e3d69aacd1bb1ea0c36151281b1e394360
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-04-14 00:29:08 +0200

    More headers

--- src/brewlog/home/views.py
+++ src/brewlog/home/views.py
@@ -1,3 +1,7 @@
+# Copyright 2012, 2019 Jarek Zgoda. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 from flask import current_app, render_template
 from flask_login import current_user
 
--- src/brewlog/models/choices.py
+++ src/brewlog/models/choices.py
@@ -1,3 +1,7 @@
+# Copyright 2012, 2019 Jarek Zgoda. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 from decimal import Decimal
 
 from flask_babel import lazy_gettext as _
