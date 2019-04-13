04a7bc5965059ad8cc01fb07b29c62b0fb5d7fb4
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-04-13 23:58:38 +0200

    Copyright dates updated

--- tests/test_brew_utils.py
+++ tests/test_brew_utils.py
@@ -1,3 +1,7 @@
+# Copyright 2012, 2019 Jarek Zgoda. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 from datetime import date, timedelta
 
 import pytest
