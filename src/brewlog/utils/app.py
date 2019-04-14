76383384f7d8ab6b73d11aee879111f7a28acb08
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-04-14 11:13:13 +0200

    Better brew navigation

--- src/brewlog/utils/app.py
+++ src/brewlog/utils/app.py
@@ -15,7 +15,3 @@ class Brewlog(Flask):
             'lstrip_blocks': True,
         })
         return options
-
-    def select_jinja_autoescape(self, filename):
-        orig_select = super().select_jinja_autoescape(filename)
-        return orig_select or filename.endswith(('.jinja', '.jinja2', '.j2'))
