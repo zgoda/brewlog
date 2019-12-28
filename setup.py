ce974a8fdd29e6943a416070b28fb38be39de51c
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-12-28 12:13:39 +0100

    Requirement spec in setup.py only

--- setup.py
+++ setup.py
@@ -25,6 +25,56 @@ def find_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
+base_reqs = [
+    'Flask',
+    'Flask-Babel',
+    'Flask-Login',
+    'Flask-FlatPages',
+    'Flask-SQLAlchemy',
+    'Flask-WTF',
+    'Bootstrap-Flask',
+    'Flask-Migrate',
+    'SQLAlchemy-Utils',
+    'Authlib[client]',
+    'Markdown',
+    'WTForms-Alchemy',
+    'WTForms-Components',
+    'permission',
+    'psycopg2-binary',
+]
+
+test_reqs = [
+    'pytest',
+    'pytest-mock',
+    'pytest-cov',
+    'pytest-flask',
+    'pytest-factoryboy',
+    'pyfakefs',
+]
+
+
+dev_reqs = [
+    'ipdb',
+    'flake8',
+    'flake8-builtins',
+    'flake8-bugbear',
+    'flake8-mutable',
+    'flake8-comprehensions',
+    'pep8-naming',
+    'dlint',
+    'doc8',
+    'rope',
+    'Sphinx',
+    'pip',
+    'setuptools',
+    'wheel',
+    'flask-shell-ipython',
+    'watchdog',
+    'termcolor',
+    'python-dotenv',
+] + test_reqs
+
+
 long_description = read('README.md')
 
 setup(
@@ -50,37 +100,14 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    install_requires=(
-        'Flask',
-        'Flask-Babel',
-        'Flask-Login',
-        'Flask-FlatPages',
-        'Flask-SQLAlchemy',
-        'Flask-WTF',
-        'Bootstrap-Flask',
-        'Flask-Migrate',
-        'SQLAlchemy-Utils',
-        'Authlib[client]',
-        'Markdown',
-        'WTForms-Alchemy',
-        'WTForms-Components',
-        'permission',
-        'psycopg2-binary',
-    ),
+    install_requires=base_reqs,
     setup_requires=(
-        'pytest-runner',
         'babel',
     ),
-    tests_require=(
-        'pytest',
-        'pytest-mock',
-        'pytest-cov',
-        'pytest-flask',
-        'pytest-factoryboy',
-        'pyfakefs',
-    ),
     extras_require={
-        'prod': ['uwsgi']
+        'prod': ['uwsgi'],
+        'test': test_reqs,
+        'dev': dev_reqs,
     },
     entry_points={
         'console_scripts': [
