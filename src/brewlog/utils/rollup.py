6b29fea079609de9bea966b79ec5eb06fed54326
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-08-02 22:01:27 +0200

    Some more documentation for Flask-Rollup

--- src/brewlog/utils/rollup.py
+++ src/brewlog/utils/rollup.py
@@ -1,5 +1,5 @@
-import hashlib
 import glob
+import hashlib
 import os
 import subprocess
 from collections import namedtuple
@@ -9,8 +9,13 @@ from typing import List, Mapping, Optional, Union
 from flask import Flask, request
 
 
-def resolve_path(root: str, path: str) -> str:
-    return os.path.normpath(os.path.abspath(os.path.join(root, path)))
+def resolve_path(*parts) -> str:
+    """Join path parts and normalise resulting path.
+
+    Returns:
+        str: normalised file system path
+    """
+    return os.path.normpath(os.path.abspath(os.path.join(*parts)))
 
 
 BundleOutput = namedtuple('BundleOutput', ['file_path', 'static_path', 'url'])
@@ -30,11 +35,18 @@ class BundleDefinitionError(RollupBundlerError):
 
 @dataclass
 class Entrypoint:
+    """Entrypoint information: path and name.
+    """
     path: str
     name: str = ''
 
-    def cmdline_param(self) -> List[str]:
-        return [f'{self.name}={self.path}']
+    def cmdline_param(self) -> str:
+        """Generate command line param from name and path.
+
+        Returns:
+            str: Rollup command line param
+        """
+        return f'{self.name}={self.path}'
 
 
 @dataclass
@@ -50,7 +62,7 @@ class Bundle:
     ``auth.login``. Upon bundling the result will be in file
     ``target_dir/auth.login.[hash].js``, and corresponding source map in file
     ``target_dir/auth.login.[hash].js.map``. This is required to use ``jsbundle`` in
-    templates and to have working aoto rebuild in development.
+    templates and to have working auto rebuild in development mode.
 
     In simplest case of single entrypoint it may be specified as string denoting path
     relative to app static folder. In any case bundle can have only one unnamed
@@ -91,6 +103,11 @@ class Bundle:
         self.entrypoints = entrypoints
 
     def resolve_paths(self, root: str):
+        """Make bundle paths absolute and normalized.
+
+        Args:
+            root: root path (application static folder path)
+        """
         for ep in self.entrypoints:
             ep.path = resolve_path(root, ep.path)
         self.target_dir = resolve_path(root, self.target_dir)
@@ -98,17 +115,36 @@ class Bundle:
             self.dependencies[index] = resolve_path(root, dep)
 
     def calc_state(self) -> str:
+        """Calculate bundle state checksum. This is used to determine if bundle should
+        be rebuild in development mode. For each input path (entrypoints and
+        dependencies) file mtime is used as a base of calculation.
+
+        Returns:
+            str: bundle state checksum
+        """
         src = [str(os.stat(ep.path).st_mtime_ns) for ep in self.entrypoints]
         src.extend([str(os.stat(d.path).st_mtime_ns) for d in self.dependencies])
         return hashlib.sha256('\n'.join(src).encode('utf-8')).hexdigest()
 
     def argv(self) -> List[str]:
+        """Return list of Rollup command line params required to build the bundle.
+
+        Returns:
+            List[str]: list of command line param tokens
+        """
         rv = ['-d', self.target_dir]
         for ep in self.entrypoints:
             rv.extend(ep.cmdline_param())
         return rv
 
     def resolve_output(self, root: str, url_path: str):
+        """Determine bundle's generation output paths (both absolute file system path
+        and relative to static folder) and url.
+
+        Args:
+            root: static content root directory (application static folder)
+            url_path: path to static content
+        """
         files = glob.glob(f'{root}/**/{self.name}.*.js')
         if len(files) == 1:
             output_path = files[0]
@@ -119,6 +155,9 @@ class Bundle:
 
 @dataclass
 class Rollup:
+    """Rollup integration with Flask. Extension can be registered in both simple way
+    and using application factory and ``init_app(app)`` pattern.
+    """
     app: Optional[Flask] = None
     bundles: Mapping[str, Bundle] = field(default_factory=dict, init=False)
     argv: List[str] = field(default_factory=list, init=False)
@@ -131,6 +170,14 @@ class Rollup:
             self.init_app(self.app)
 
     def init_app(self, app: Flask):
+        """Initialise application. This function sets up required configuration
+        defaults and initial Rollup command line args. In non-production mode
+        autorebuild is enabled. Template function ``jsbundle`` is registered here
+        as Jinja2 global object.
+
+        Args:
+            app: application object
+        """
         self.mode_production = os.environ.get('FLASK_ENV', 'production') == 'production'
         self.static_folder = app.static_folder
         self.static_url_path = app.static_url_path
@@ -154,15 +201,26 @@ class Rollup:
             return bundle.output.url
 
     def register(self, bundle: Bundle):
+        """Register bundle. At this moment input paths are resolved.
+
+        Args:
+            bundle: bundle object to be registered
+        """
         self.bundles[bundle.name] = bundle
         bundle.resolve_paths(self.static_folder)
 
     def run_rollup(self, bundle_name: str):
+        """Run Rollup bundler over specified bundle is bundle state changed. Once
+        Rollup finishes bundle's output is resolved (paths and url).
+
+        Args:
+            bundle_name: name of the bundle to be rebuilt
+        """
         bundle = self.bundles[bundle_name]
         new_state = bundle.calc_state()
         if bundle.state != new_state:
             argv = self.argv.copy()
-            argv.extend(bundle.argv())
+            argv.append(bundle.argv())
             environ = os.environ.copy()
             environ['NODE_ENV'] = environ['FLASK_ENV']
             kw = {}
