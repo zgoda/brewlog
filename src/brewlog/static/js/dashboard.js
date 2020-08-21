0d63aa08a619eb3645304267cf9a7265035cdfd4
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-08-21 17:27:40 +0200

    Action form in modal

--- src/brewlog/static/js/dashboard.js
+++ src/brewlog/static/js/dashboard.js
@@ -3,16 +3,14 @@ import { h, render } from 'preact';
 import { useState } from 'preact/hooks';
 
 const FermentingItem = (props) => {
+  const [showModal, setModal] = useState(false);
+  const [op, setOp] = useState('');
+
+  const onClose = () => setModal(false);
 
   const toggleOp = (op) => {
-    props.setOp(op);
-    const nextState = !props.formVisible;
-    props.formToggle(nextState);
-    if (nextState) {
-      props.setCurItem(props.data.id);
-    } else {
-      props.setCurItem(0);
-    }
+    setOp(op);
+    setModal(true);
   };
 
   return (
@@ -22,96 +20,99 @@ const FermentingItem = (props) => {
         <span class="ml-2">
           <button class="button is-small is-primary is-light mr-2" onClick={() => toggleOp('transfer')}>przelej</button>
           <button class="button is-small is-primary is-light mr-2" onClick={() => toggleOp('package')}>rozlej</button>
-          <a class="button is-small is-primary is-light" href="{props.data.url}">edycja</a>
+          <a class="button is-small is-primary is-light" href={props.data.url}>edycja</a>
         </span>
       </div>
+      {showModal && (
+        <ActionForm
+          setModal={setModal}
+          showModal={showModal}
+          onClose={onClose}
+          op={op}
+          csrfToken={props.token}
+          brew={props.data}
+        />
+      )}
     </div>
   )
 }
 
 const ActionForm = (props) => {
-  const opLabels = {
-    transfer: 'przelej',
-    package: 'rozlej'
-  };
-  let formClass = '';
-  if (!props.visible) {
-    formClass = 'is-hidden';
-  }
-
-  return (
-    <div class={formClass}>
-      <p class="has-text-weight-bold">{opLabels[props.op]}</p>
-      <form onSubmit={props.onSubmit}>
-        <div class="field">
-          <label class="label">Gęstość</label>
-          <div class="control">
-            <input class="input" type="number" name="fg" step="0.1" onInput={props.setFg} value={props.fg} />
-          </div>
-        </div>
-        <div class="field">
-          <label class="label">Data</label>
-          <div class="control">
-            <input class="input" type="date" name="date" onInput={props.setDate} value={props.date} />
-          </div>
-        </div>
-        <div class="field">
-          <label class="label">Notatki</label>
-          <div class="control">
-            <textarea class="textarea" name="notes" onInput={props.setNotes}>{props.notes}</textarea>
-          </div>
-        </div>
-        <div class="field">
-          <div class="control">
-            <button type="submit" class="button is-primary">wyślij</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  )
-}
-
-const Fermenting = ({ brews, csrfToken }) => {
-  const [fg, setFg] = useState('');
+  const [fg, setFg] = useState(0);
   const [date, setDate] = useState('');
   const [notes, setNotes] = useState('');
-  const [formVisible, setFormVisible] = useState(false);
-  const [op, setOp] = useState('');
-  const [curItem, setCurItem] = useState(0);
 
-  const changeFg = (event) => {
-    setFg(event.target.value);
-  }
+  const opLabels = {
+    transfer: `Przelej ${props.brew.name}`,
+    package: `Rozlew ${props.brew.name}`
+  };
 
-  const changeDate = (event) => {
-    setDate(event.target.value);
-  }
-
-  const changeNotes = (event) => {
-    setNotes(event.target.value);
+  let modalClass = 'modal';
+  if (props.showModal) {
+    modalClass = 'modal is-active';
   }
 
   const onSubmit = (event) => {
     event.preventDefault();
-    const url = `/brew/api/${op}`;
+    const url = `/brew/api/${props.op}`;
     fetch(url, {
       method: 'POST',
       headers: {
-        'X-CSRFToken': csrfToken,
+        'X-CSRFToken': props.csrfToken,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ fg, date, notes, pk: curItem })
+      body: JSON.stringify({ fg, date, notes, pk: props.brew.id })
     })
       .then((resp) => {
         if (!resp.ok) {
           throw new Error(`Response: ${resp.status}`);
         }
         resp.json();
+        props.onClose();
       })
       .then((data) => console.log(data))
       .catch((err) => console.error('HTTP fetch error:', err));
   };
 
+  return (
+    <div class={modalClass}>
+      <div class="modal-background" />
+      <div class="modal-content">
+        <div class="box">
+          <p class="has-text-weight-bold">{opLabels[props.op]}</p>
+          <form onSubmit={onSubmit}>
+            <div class="field">
+              <label class="label">Gęstość</label>
+              <div class="control">
+                <input class="input" type="number" name="fg" step="0.1" onInput={setFg} value={fg} />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Data</label>
+              <div class="control">
+                <input class="input" type="date" name="date" onInput={setDate} value={date} />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Notatki</label>
+              <div class="control">
+                <textarea class="textarea" name="notes" onInput={setNotes}>{notes}</textarea>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <button type="submit" class="button is-primary">wyślij</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" onClick={() => props.onClose()} />
+    </div>
+  )
+}
+
+const Fermenting = ({ brews, csrfToken }) => {
   return (
     <div class="column">
       <div class="box">
@@ -120,23 +121,9 @@ const Fermenting = ({ brews, csrfToken }) => {
           <FermentingItem
             data={brew}
             key={brew.id}
-            formToggle={setFormVisible}
-            setCurItem={setCurItem}
-            formVisible={formVisible}
-            setOp={setOp}
+            token={csrfToken}
           />
         ))}
-        <ActionForm
-          fg={fg}
-          setFg={changeFg}
-          date={date}
-          setDate={changeDate}
-          notes={notes}
-          setNotes={changeNotes}
-          onSubmit={onSubmit}
-          visible={formVisible}
-          op={op}
-        />
       </div>
     </div>
   );
